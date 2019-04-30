82d034718863edc6038284b7eb71cd3b657e00ed
jzq <juzeqian@mail.ustc.edu.cn>
2019-04-30 15:23:25 +0800

    @remote_test

--- docs/files/rain_@remote_test/@remote_prime.py
+++ docs/files/rain_@remote_test/@remote_prime.py
@@ -3,12 +3,14 @@ import operator
 import json
 import profile
 import pstats
+import math
 def is_prime(n):
 	if n <= 1:
 		return False
 	if n % 2 == 0:
 		return False
-	for i in range(3, n//2 + 1,2):
+	k = int(math.sqrt(n))+1;
+	for i in range(3, k,2):
 		if n % i == 0:
 			return False
 	return True 
